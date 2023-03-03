2672bd6fee578db3e69036f3c0a5d4bf4c54fb94
J-Carlos99 <87335255+J-Carlos99@users.noreply.github.com>
2023-03-02 23:40:52 -0300

    Create CursoJava.java

---
+++ CursoJava.java
@@ -0,0 +1,16 @@
+class CursoJava {
+  public static void main(String[] args) {
+    int nota = 85;
+    int faltas = 10;
+    int maxFaltas = 5;
+    int media = 60;
+    if(nota >= media && faltas <= maxFaltas){
+      System.out.println("Aprovado");
+    }else if(nota >= 40){
+      System.out.println("Reprovado");
+    }else{
+      System.out.println("Reprovado");
+    }
+    System.out.println("Fim de programa");
+  }
+}
